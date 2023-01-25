a86888e7e4f100c57a1e46d429a4ad61ce873fb0
borutp <borut.pezdicek@hse.si>
2023-01-25 11:55:32 +0100

    update

--- config.h
+++ config.h
@@ -12,23 +12,6 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
 
 #define EE_HANDS
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
 
 #define MATRIX_ROWS 8 // Rows are doubled-up.
 #define MATRIX_COLS 5
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
